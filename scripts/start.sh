011d8a28bd219ef271cd07587d4e58a67ffd0882
cb218 <66742464+cb218@users.noreply.github.com>
2021-10-11 15:14:11 -0500

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /home/ec2-user/.nvm/versions/node/v16.11.0/bin/pm2
-pm2 start /home/ubuntu/quoterepo/serverSide/server.js
+cd /home/ubuntu/quoterepo/
+sudo pm2 start serverSide/server.js
