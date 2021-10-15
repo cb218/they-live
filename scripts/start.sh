544249db0a26db8f71afd68605dd4d9b95247d12
cb218 <66742464+cb218@users.noreply.github.com>
2021-10-15 07:21:06 -0500

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,4 @@
-#!/bin/bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+source /home/ec2-user/.bash_profile
 
 cd /home/ec2-user/quoterepo/serverSide
 pm2 start server.js
