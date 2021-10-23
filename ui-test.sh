917e0975deb0e8bb49b5a9a055c81314ec877d98
cb218 <cb218@students.uwf.edu>
2021-10-23 17:28:39 -0500

    testing ui

--- ui-test.sh
+++ ui-test.sh
@@ -3,26 +3,28 @@ curl http://52.6.144.203:3000/ > welcomepage.txt
 curl http://52.6.144.203:3000/search?search_string=money > moneysearch.txt
 curl http://52.6.144.203:3000/search?search_string=asdfghjk > asdfghjk.txt
 
-if [ (grep "Quote Server" welcomepage.txt) == 0 ]
-then
-  echo "Test 1 passed, server up and running."
-else
-  echo "Test 1 failed, could not locate server."
-  exit 1
-fi
+grep "Quote Server" welcomepage.txt 
 
-if [ (grep "money" moneysearch.txt) == 0 ]
-then
-  echo "Test 2 passed, 'money' search successful."
-else
-  echo "Test 2 failed, 'money' search failed."
-  exit 1
-fi
+# if [ grep "Quote Server" welcomepage.txt == 0 ]
+# then
+#   echo "Test 1 passed, server up and running."
+# else
+#   echo "Test 1 failed, could not locate server."
+#   exit 1
+# fi
 
-if [ (grep "No matching quote found" asdfghjk.txt) == 0 ]
-then
-  echo "Test 3 passed, fake word search unsuccessful."
-else
-  echo "Test 3 failed, fake word search failed."
-  exit 1
-fi
+# if [ (grep "money" moneysearch.txt) == 0 ]
+# then
+#   echo "Test 2 passed, 'money' search successful."
+# else
+#   echo "Test 2 failed, 'money' search failed."
+#   exit 1
+# fi
+
+# if [ (grep "No matching quote found" asdfghjk.txt) == 0 ]
+# then
+#   echo "Test 3 passed, fake word search unsuccessful."
+# else
+#   echo "Test 3 failed, fake word search failed."
+#   exit 1
+# fi
