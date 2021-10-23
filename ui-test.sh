9fe9fa85d0ee56e2a9831781e04985d3d21bcee7
cb218 <cb218@students.uwf.edu>
2021-10-23 17:40:36 -0500

    fixed ui test

--- ui-test.sh
+++ ui-test.sh
@@ -3,31 +3,13 @@ curl http://52.6.144.203:3000/ > welcomepage.txt
 curl http://52.6.144.203:3000/search?search_string=money > moneysearch.txt
 curl http://52.6.144.203:3000/search?search_string=asdfghjk > asdfghjk.txt
 
-grep "Quote Server" welcomepage.txt 
+grep "Quote Server" welcomepage.txt
+echo "Test 1 passed, server up and running."
+
 grep "money" moneysearch.txt
-grep "Noogalaboo" asdfghjk.txt
+echo "Test 2 passed, 'money' search successful."
 
+grep "No matching quote found" asdfghjk.txt
+echo "Test 3 passed, fake word search unsuccessful."
 
-# if [ grep "Quote Server" welcomepage.txt == 0 ]
-# then
-#   echo "Test 1 passed, server up and running."
-# else
-#   echo "Test 1 failed, could not locate server."
-#   exit 1
-# fi
-
-# if [ (grep "money" moneysearch.txt) == 0 ]
-# then
-#   echo "Test 2 passed, 'money' search successful."
-# else
-#   echo "Test 2 failed, 'money' search failed."
-#   exit 1
-# fi
-
-# if [ (grep "No matching quote found" asdfghjk.txt) == 0 ]
-# then
-#   echo "Test 3 passed, fake word search unsuccessful."
-# else
-#   echo "Test 3 failed, fake word search failed."
-#   exit 1
-# fi
+echo "Deploying to production environment!"
