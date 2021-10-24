ef7178c78751c50385d4439d7587aebabb1e0b7b
cb218 <cb218@students.uwf.edu>
2021-10-23 21:07:24 -0500

    prep for demo 4

--- ui-test.sh
+++ ui-test.sh
@@ -21,7 +21,7 @@ else
   echo "Test 2 passed, 'money' search successful."
 fi
 
-TEST3VAL=$(grep -c "No matching quote found" asdfghjk.txt)
+TEST3VAL=$(grep -c "No matching quote was found" asdfghjk.txt)
 if [ $TEST3VAL == 0 ]
 then
   echo "Test 3 failed, fake word search failed."
