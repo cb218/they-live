7f534b343aea2e2e72350635fd1c3ad9dcbde917
cb218 <cb218@students.uwf.edu>
2021-10-23 20:55:33 -0500

    demo 4 failure

--- ui-test.sh
+++ ui-test.sh
@@ -3,15 +3,6 @@ curl http://52.6.144.203:3000/ > welcomepage.txt
 curl http://52.6.144.203:3000/search?search_string=money > moneysearch.txt
 curl http://52.6.144.203:3000/search?search_string=asdfghjk > asdfghjk.txt
 
-# grep "Quote Server" welcomepage.txt
-# echo "Test 1 passed, server up and running."
-
-# grep "money" moneysearch.txt
-# echo "Test 2 passed, 'money' search successful."
-
-# grep "Noogalaboo" asdfghjk.txt
-# echo "Test 3 passed, fake word search unsuccessful."
-
 TEST1VAL=$(grep -c "Quote Server" welcomepage.txt)
 if [ $TEST1VAL == 0 ]
 then
