2201f017b88b9843fd173cc3db0cedb758146cd7
cb218 <cb218@students.uwf.edu>
2021-10-23 20:36:37 -0500

    reorder

--- ui-test.sh
+++ ui-test.sh
@@ -13,32 +13,31 @@ curl http://52.6.144.203:3000/search?search_string=asdfghjk > asdfghjk.txt
 # echo "Test 3 passed, fake word search unsuccessful."
 
 TEST1VAL=$(grep -c "Quote Server" welcomepage.txt)
-echo $TEST1VAL
-if [ $TEST1VAL > 0 ]
+if [ $TEST1VAL == 0 ]
 then
-  echo "Test 1 passed, server up and running."
-else
   echo "Test 1 failed, could not locate server."
   exit 1
+else
+  echo "Test 1 passed, server up and running."
 fi
 
 TEST2VAL=$(grep -c "money" moneysearch.txt)
-if [ $TEST2VAL > 0 ]
+if [ $TEST2VAL == 0 ]
 then
-  echo "Test 2 passed, 'money' search successful."
-else
   echo "Test 2 failed, 'money' search failed."
   exit 1
+else
+  echo "Test 2 passed, 'money' search successful."
 fi
 
 #TEST3VAL=$(grep -c "No matching quote found" asdfghjk.txt)
 TEST3VAL=$(grep -c "oogalaboo" asdfghjk.txt)
-if [ $TEST3VAL > 0 ]
+if [ $TEST3VAL == 0 ]
 then
-  echo "Test 3 passed, fake word search unsuccessful."
-else
   echo "Test 3 failed, fake word search failed."
   exit 1
+else
+  echo "Test 3 passed, fake word search unsuccessful."
 fi
 
 echo "Deploying to production environment!"
